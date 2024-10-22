de56ea42e05020275fb52cb2beb91fa00d4adde5
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-10-22 08:02:23 +0530

    color_coding_function added

--- Sql_bash_scripts/multiple_db_connect.sh
+++ Sql_bash_scripts/multiple_db_connect.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-echo "\e[1;32m Script executed at:" $(date '+%Y-%m-%d %H:%M:%S')
-echo "You are working with app-service root-user to connect src (OR) dest databases. Please select based on your needs."
+echo "Color coding for a shell script..."
+
+# Function to print green text
+green() {
+    echo -e "\e[1;32mThis text is green\e[0m"
+}
+
+# Function to print yellow text
+red() {
+    echo -e "\e[1;31mThis text is yellow\e[0m"
+}
+
+# Execute color functions
+green
+red
+
+# Print execution timestamp
+echo "green() Script executed at: $(date '+%Y-%m-%d %H:%M:%S')"
+echo "You are working with app-service root-user to connect to src (OR) dest databases. Please select based on your needs."
 
 # Variables
 src_db="app1.ap-south-1.rds.amazonaws.com"
@@ -11,22 +28,22 @@ username="dbadmin postgres"
 
 # Function to connect to the source database
 connect_src() {
-    echo "\e[1;32m Connecting to the source database $src_db"
-    psql -h $src_db -p $port -U $username
+    echo "green() Connecting to the source database $src_db"
+    psql -h "$src_db" -p "$port" -U "$username"
 }
 
 # Function to connect to the destination database
 connect_dest() {
-    echo "\e[1;32m Connecting to the destination database $dest_db"
-    psql -h $dest_db -p $port -U $username
+    echo "green() Connecting to the destination database $dest_db"
+    psql -h "$dest_db" -p "$port" -U "$username"
 }
 
 # Prompt user for input
-echo "\e[1;32m Which database do you want to connect to? (src/dest)"
+echo "green() Which database do you want to connect to? (src/dest)"
 read -r db_choice
 
 # Print the selected database choice
-echo "\e[1;33m 'You have selected db choice as: $db_choice and the db endpoint to connect is': $dest_db "
+echo "green() You have selected db choice as: $db_choice and the db endpoint to connect is: $dest_db"
 
 # Use case statement to handle user input
 case $db_choice in
@@ -37,6 +54,6 @@ case $db_choice in
         connect_dest
         ;;
     *)
-        echo "\e[1;31m Invalid choice. Please choose 'src' or 'dest'."
+        red "Invalid choice. Please choose 'src' or 'dest'."
         ;;
 esac
